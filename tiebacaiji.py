a0c06dcf9c67517d2ca83a96c8ed4f1709ef4146
sure15 <gxishk15@gmail.com>
2015-04-24 23:28:44 +0800

    采集贴吧图片
    采集贴吧单个页面的图片

---
+++ tiebacaiji.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+import re,urllib,urllib2,cookielib
+
+headers={
+		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Cookie':'',
+		'Host':'tieba.baidu.com',
+		'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+}
+url=raw_input('url:')
+request=urllib2.Request(url,headers=headers)
+data=urllib2.urlopen(request).read()
+picurl=re.findall(r'<img class="BDE_Image" src="(.*?)" pic_ext="jpeg"',data)
+i=0
+for x in picurl:
+	filename=str(i)+'.jpg'
+	data1=urllib2.urlopen(x).read()
+	file=open(filename,'wb')
+	file.write(data1)
+	file.close()
+	i=i+1
+print 'finished!'
+	
